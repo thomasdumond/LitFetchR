1975e409402bc0b6f184f34e7ce081cafd939ef4
Thomas Dumond <a1224158@adelaide.edu.au>
2025-11-30 15:30:44 +1030

    documenting and commenting create_id_history

--- R/create_id_history.R
+++ R/create_id_history.R
@@ -1,24 +1,26 @@
-#' creates an excel file to store the references' identification retrieved at each search
-#' @return create an excel file
+#' Creates an excel file to store the references identification retrieved at each search
+#'
+#' @return history_id.xlsx
 
 # create history if does not exist
 create_id_history <- function(){
 
   if (!file.exists("history_id.xlsx")){
-    history_id <- openxlsx::createWorkbook()
-    openxlsx::addWorksheet(history_id, "updated_id_list")
+    history_id <- openxlsx::createWorkbook() #create the excel workbook in R
+    openxlsx::addWorksheet(history_id, "updated_id_list") #add a sheet
     openxlsx::writeData(history_id,
                         sheet = "updated_id_list",
                         x = "id",
                         startCol = 1,
-                        startRow = 1)
-    openxlsx::saveWorkbook(history_id, "history_id.xlsx")
+                        startRow = 1) #column name is "id"
+    openxlsx::saveWorkbook(history_id, "history_id.xlsx") #save the R workbook to an excel file
   } else{
+    # If the ID history already exist, just load it to an R workbook object
     message("File already exists")
     history_id <- openxlsx::loadWorkbook("history_id.xlsx")
   }
 
-  # return history_search and sheet_name in a list
+  # Returns the R workbook object named history_id
   return(list(
     history_id = history_id
   ))

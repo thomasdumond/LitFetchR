8134cdff95a7623ee69b9701737703290c0ac3a6
Thomas Dumond <a1224158@adelaide.edu.au>
2025-11-30 15:25:00 +1030

    documentation and comments create_dedup_history

--- R/create_dedup_history.R
+++ R/create_dedup_history.R
@@ -1,21 +1,25 @@
-#' creates an excel file to store the deduplication history
-#' @return create an excel file
+#' Creates an excel file to store the deduplication history
+#'
+#' @return history_dedup.xlsx
 
 create_dedup_history <- function(){
-  date_suffix <- format(Sys.time(), "%Y-%m-%d-%H%M%S")
-  history_dedup <- openxlsx::createWorkbook()
-  openxlsx::addWorksheet(history_dedup, "citations")
-  openxlsx::addWorksheet(history_dedup, "auto_dedup_unique")
-  openxlsx::addWorksheet(history_dedup, "manual_dedup")
-  openxlsx::addWorksheet(history_dedup, "unique_citations")
-  hist_dedup_name <- paste0("history_dedup_", date_suffix,".xlsx")
-  openxlsx::saveWorkbook(history_dedup, hist_dedup_name)
 
-  # return history_search and sheet_name in a list
-  return(list(
-    history_dedup = history_dedup,
-    hist_dedup_name = hist_dedup_name
-    )
-  )
+  #setup the R object representing the excel file
+  date_suffix <- format(Sys.time(), "%Y-%m-%d-%H%M%S") #setup a unique name
+  history_dedup <- openxlsx::createWorkbook() #create the excel workbook in R
+  openxlsx::addWorksheet(history_dedup, "citations") #add a sheet
+  openxlsx::addWorksheet(history_dedup, "auto_dedup_unique") #add a sheet
+  openxlsx::addWorksheet(history_dedup, "manual_dedup") #add a sheet
+  openxlsx::addWorksheet(history_dedup, "unique_citations") #add a sheet
+
+  #create the history deduplication excel file
+  hist_dedup_name <- paste0("history_dedup_", date_suffix,".xlsx") #full name of the excel file
+  openxlsx::saveWorkbook(history_dedup, hist_dedup_name) #save the R workbook to an excel file
+
+  # returns the R workbook object and the name of the excel file in a list
+  return(list(history_dedup = history_dedup,
+              hist_dedup_name = hist_dedup_name
+              )
+         )
 
 }

5f8670643cf713ce884ae73e3ab489658a01318a
Thomas Dumond <a1224158@adelaide.edu.au>
2025-12-16 15:13:08 +1030

    zzz supression

--- R/zzz.R
+++
@@ -1,9 +0,0 @@
-# Set default options when package is loaded
-.onLoad <- function(libname, pkgname) {
-  op.asysd <- list(
-    shiny.launch.browser = TRUE  # always open Shiny in browser
-  )
-  toset <- !(names(op.asysd) %in% names(options()))
-  if (any(toset)) options(op.asysd[toset])
-  invisible()
-}
